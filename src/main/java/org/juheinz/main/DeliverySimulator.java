0397042e4cc09a7760950ae12fcf19fb9f50fdb1
julia <julia-heinzelmann@gmx.net>
2023-12-13 14:15:23 +0100

    Homework 7: started implementing internal DSL

---
+++ src/main/java/org/juheinz/main/DeliverySimulator.java
@@ -0,0 +1,64 @@
+package org.juheinz.main;
+
+import org.juheinz.appserver.UserNotifier;
+import org.juheinz.appserver.UserRepository;
+import org.juheinz.deliveryserver.ParcelManager;
+import org.juheinz.deliveryserver.ParcelRepository;
+import org.juheinz.deliveryserver.RouteService;
+import org.juheinz.navigation.Navigator;
+import org.juheinz.scanner.ParcelScanner;
+import org.juheinz.entities.StaffDeployment;
+import org.juheinz.utility.Logger;
+
+import java.time.LocalDateTime;
+
+/**
+ * Simulates activities from scanner. Packages get loaded and delivered.
+ */
+public class DeliverySimulator {
+    Logger logger;
+
+    public void startSimulation() throws InterruptedException {
+        Navigator navigator = Navigator.getInstance();
+        RouteService routeService = RouteService.getInstance(navigator);
+
+        UserRepository userRepository = UserRepository.getInstance();
+        userRepository.connectToDatabse("0908-07.appusers.de", "user");
+
+        UserNotifier userNotifier = UserNotifier.getInstance(userRepository);
+
+        ParcelRepository parcelRepository = ParcelRepository.getInstance(userNotifier);
+        parcelRepository.connectToDatabse("012-34.packages.de", "package");
+
+        ParcelManager parcelManager = ParcelManager.getInstance(parcelRepository, routeService);
+        ParcelScanner parcelScanner = ParcelScanner.getInstance(parcelManager);
+        logger = new Logger("main");
+
+        setStaffDeployment();
+
+
+        // simulate scanner activity
+        logger.log("van is being loaded", "meta");
+        int[] parcelCode = new int[]{8, 7, 6, 5, 4, 3, 1};
+        for (int code : parcelCode) {
+            parcelScanner.scanParcelAsLoaded(code, LocalDateTime.now());
+            Thread.sleep(1000);
+        }
+        logger.log("van is rolling out", "meta");
+        Thread.sleep(2000);
+        for (int code : parcelCode) {
+            parcelScanner.scanParcelAsDelivered(code, LocalDateTime.now());
+            Thread.sleep(1000);
+        }
+    }
+
+    /**
+     * Example for an interal Domain Specific Language (DSL).
+     */
+    private void setStaffDeployment(){
+        StaffDeployment staffDeployment = new StaffDeployment(1,2,3);
+        staffDeployment.weatherCondition("sonnig").fuelPercentage(98).staffStatus("gesund");
+        logger.log(staffDeployment.toString(), "admin");
+    }
+
+}
--- src/main/java/org/juheinz/main/Main.java
+++ src/main/java/org/juheinz/main/Main.java
@@ -1,48 +1,11 @@
 package org.juheinz.main;
 
 
-import org.juheinz.appserver.UserNotifier;
-import org.juheinz.appserver.UserRepository;
-import org.juheinz.deliveryserver.ParcelManager;
-import org.juheinz.deliveryserver.ParcelRepository;
-import org.juheinz.deliveryserver.RouteService;
-import org.juheinz.navigation.Navigator;
-import org.juheinz.scanner.ParcelScanner;
-import org.juheinz.utility.Logger;
-
-import java.time.LocalDateTime;
-
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        Navigator navigator = Navigator.getInstance();
-        RouteService routeService = RouteService.getInstance(navigator);
-
-        UserRepository userRepository = UserRepository.getInstance();
-        userRepository.connectToDatabse("0908-07.appusers.de", "user");
-
-        UserNotifier userNotifier = UserNotifier.getInstance(userRepository);
-
-        ParcelRepository parcelRepository = ParcelRepository.getInstance(userNotifier);
-        parcelRepository.connectToDatabse("012-34.packages.de", "package");
-
-        ParcelManager parcelManager = ParcelManager.getInstance(parcelRepository, routeService);
-        ParcelScanner parcelScanner = ParcelScanner.getInstance(parcelManager);
-
-        Logger logger = new Logger("main");
-        // simulate scanner activity
-        logger.log("van is being loaded", "meta");
-        int[] parcelCode = new int[]{8, 7, 6, 5, 4, 3, 1};
-        for (int code : parcelCode) {
-            parcelScanner.scanParcelAsLoaded(code, LocalDateTime.now());
-            Thread.sleep(1000);
-        }
-        logger.log("van is rolling out", "meta");
-        Thread.sleep(2000);
-        for (int code : parcelCode) {
-            parcelScanner.scanParcelAsDelivered(code, LocalDateTime.now());
-            Thread.sleep(1000);
-        }
+       DeliverySimulator deliverySimulator = new DeliverySimulator();
+       deliverySimulator.startSimulation();
 
     }
 }
